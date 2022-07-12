e9ecf20dc76d459539659c9da4e2bb483b251010
Emmanuel López <emmlop243@gmail.com>
2022-07-12 14:50:18 -0500

    Configuración inicial del proyecto: Vue 2 + Vuetify 2 y customización de la fuente

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-main>
+      <v-container class="text-center mt-5 body-1">
+        <h1 class="text-h2">Aquí comienza todo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus explicabo id
+          iusto natus possimus quam quas ratione rem voluptates!
+        </p>
+        <v-icon>fa fa-user</v-icon>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
