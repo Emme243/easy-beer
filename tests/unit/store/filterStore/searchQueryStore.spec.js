9d13a71fd0fbd6458fa37f40860d395a68225ff2
Emmanuel López <emmlop243@gmail.com>
2022-07-17 14:27:37 -0500

    Se realizaron las pruebas para el filtro de búsqueda por mes inicial y final de elaboración de cerveza (brewedDateStore)

--- tests/unit/store/filterStore/searchQueryStore.spec.js
+++ tests/unit/store/filterStore/searchQueryStore.spec.js
@@ -1,6 +1,8 @@
 import store from '@/store';
 
 describe('Vuex - Pruebas en el estado de búsqueda por nombre de cervezas', function () {
+  let beerSearchQuery = 'Pilsner';
+
   // Base
   it('Debe haber un valor vacío en el estado de búsqueda por nombre de cervezas', function () {
     const { searchQuery } = store.state.filter.searchQuery;
@@ -9,7 +11,6 @@ describe('Vuex - Pruebas en el estado de búsqueda por nombre de cervezas', func
 
   // Mutations
   it('Debe poder setear un valor de búsqueda mediante la mutación setSearchQuery', function () {
-    const beerSearchQuery = 'Pilsen';
     store.commit('filter/setSearchQuery', beerSearchQuery);
     const { searchQuery } = store.state.filter.searchQuery;
     expect(searchQuery).toBe(beerSearchQuery);
@@ -17,7 +18,6 @@ describe('Vuex - Pruebas en el estado de búsqueda por nombre de cervezas', func
 
   // Actions
   it('Debe poder setear un valor de búsqueda mediante la acción setSearchQuery', function () {
-    const beerSearchQuery = 'Pilsen';
     store.dispatch('filter/setSearchQuery', beerSearchQuery);
     const { searchQuery } = store.state.filter.searchQuery;
     expect(searchQuery).toBe(beerSearchQuery);
@@ -32,7 +32,6 @@ describe('Vuex - Pruebas en el estado de búsqueda por nombre de cervezas', func
 
   // Getters
   it('Debe poder obtener el valor de búsqueda mediante el getter searchQuery', function () {
-    const beerSearchQuery = 'Pilsen';
     store.commit('filter/setSearchQuery', beerSearchQuery);
     const searchQuery = store.getters['filter/searchQuery'];
     expect(searchQuery).toBe(beerSearchQuery);
